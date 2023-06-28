2eb4e1f3300a9dd6c2339048f2a0ec783750de74
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-06-28 09:46:10 -0600

    update

--- _misael_main.py
+++ _misael_main.py
@@ -13,42 +13,26 @@ from optim_utils import *
 # opt.make_plot(global_res_df, local_res_df, mbounds=plot_bounds, angle=[45,225],
 #               showcontours=True, showtrajectory=True)
 
-###########################################################################################################
-######################################### Brute Force optimization ########################################
-###########################################################################################################
+##### Brute Force optimization #####
 # result_presWAT = run_BruteForce_opt(measure_type=1)
 # result_co2sl   = run_BruteForce_opt(measure_type=2)
 # result_temp    = run_BruteForce_opt(measure_type=3)
 # reuslt_presWAT_co2sl = run_BruteForce_opt(measure_type=4)
 
-###########################################################################################################
-########################################### Layer optimization ###########################################
-###########################################################################################################
-# run_layer_opt(measure_type=1, layer=1)
-# run_layer_opt(measure_type=1, layer=2)
-# run_layer_opt(measure_type=1, layer=3)
+##### Layer optimization #####
+# run_layer_opt(measure_type=1, layer=1); run_layer_opt(measure_type=1, layer=2); run_layer_opt(measure_type=1, layer=3)
+# run_layer_opt(measure_type=2, layer=1); run_layer_opt(measure_type=2, layer=2); run_layer_opt(measure_type=2, layer=3)
+# run_layer_opt(measure_type=3, layer=1); run_layer_opt(measure_type=3, layer=2); run_layer_opt(measure_type=3, layer=3)
+# run_layer_opt(measure_type=4, layer=1); run_layer_opt(measure_type=4, layer=2); run_layer_opt(measure_type=4, layer=3)
 
-# run_layer_opt(measure_type=2, layer=1)
-# run_layer_opt(measure_type=2, layer=2)
-# run_layer_opt(measure_type=2, layer=3)
+##### Column optimization #####
+# run_column_opt(measure_type=1); run_column_opt(measure_type=2); run_column_opt(measure_type=3); run_column_opt(measure_type=4)    
 
-# run_layer_opt(measure_type=3, layer=1)
-# run_layer_opt(measure_type=3, layer=2)
-# run_layer_opt(measure_type=3, layer=3)
 
-# run_layer_opt(measure_type=4, layer=1)
-# run_layer_opt(measure_type=4, layer=2)
-# run_layer_opt(measure_type=4, layer=3)
-
-###########################################################################################################
-########################################### Column optimization ###########################################
-###########################################################################################################
-# run_column_opt(measure_type=1)
-# run_column_opt(measure_type=2)
-# run_column_opt(measure_type=3)
-# run_column_opt(measure_type=4)    
-
+#########################################################################################################
+#########################################################################################################
 ### LAYER OPTIMIZATION
+'''
 layer1, layer2, layer3 = np.arange(2,18), np.arange(18,34), np.arange(34,50)
 layer_list = [layer1, layer2, layer3]
 result = pd.DataFrame(index=['layer 1', 'layer 2', 'layer 3'], columns=['measure 1', 'measure 2', 'measure 3', 'measure 4'])
@@ -58,6 +42,7 @@ for m in range(4):
         print('layer {} DONE'.format(l))
     print('measure {} DONE'.format(m))
 result.to_csv('optimization_by_layer.csv')
+'''
 
 ### COLUMN/WELL OPTIMIZATION
 well_names = ['well {}'.format(p+1) for p in range(16)]
@@ -68,7 +53,10 @@ wells = np.array(list(well.values()))
 results = pd.DataFrame(index=well_names, columns=['measure 1', 'measure 2', 'measure 3', 'measure 4'])
 for m in range(4):
     for i in range(16):
-        results.iloc[i,m] = Proxy(ncol_data=list(wells[i]), measure_type=m+1, rom_data=LinearRegression(), rom_obj=LinearRegression(), verbose=True).value
+        results.iloc[i,m] = Proxy(ncol_data=list(wells[i]), measure_type=m+1, nMCSamples=10000, rom_data=Earth(), rom_obj=Earth(), verbose=True).value
         print('column/well {} DONE'.format(i))
     print('measure {} DONE'.format(m))
-results.to_csv('optimizabion_by_column.csv')
+results.to_csv('optimization_by_column.csv')
+
+#########################################################################################################
+#########################################################################################################
